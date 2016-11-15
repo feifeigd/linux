bb3fe1e9a8d208d166336a380b1a2fee00761bfe
luofei <13632323692@139.com>
2016-11-15 13:38:56 +0800

    子进程退出的异步善后处理

--- zombie/src/main.cpp
+++ zombie/src/main.cpp
@@ -1,21 +1,53 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <sys/wait.h>	// waitpid
+#include <bits/sigset.h>	// sigset_t
 using namespace std;
 
-int main(){
-	int i = 1;
+void sigchld_handler(int signo);
+	
+int main(){	
 	cout << "The main program process ID is " << getpid() << endl;
-	pid_t child_pid = fork();	// 创建子进程, 返回子进程id
-	if (child_pid)	// 
+	struct sigaction newact, oldact;
+	newact.sa_handler = sigchld_handler;	// 设置信号处理程序
+	newact.sa_flags = 0;	// 无特殊选项
+	if (-1 == sigemptyset(&newact.sa_mask)
+	    || -1 == sigaction(SIGCHLD, &newact, &oldact))// 子进程状态发生变化
+		perror("Failed to set SIGCHLD.");
+	for (size_t i = 0; i < 2; ++i)	// fork 5个子进程
 	{
-		cout << "Main thread sleep 60 second." << endl;
-		sleep(60);
-	}
-	else
-	{
-		cout << "Child thread exit." << endl;
+		pid_t child_pid = fork();	// 创建子进程, 返回子进程id
+		if (child_pid)	// 
+		{
+			cout << "This is the parent process, with id " << getpid() << endl;
+			cout << "Main thread sleep 60 second." << endl;
+			sleep(20);
+		}
+		else
+		{
+			cout << "This is the child process, with id " << getpid() << endl;
+			cout << "Child thread exit." << endl;
+			break;
+		}
 	}
 	
 	return 0;
+}
+
+//子进程退出的异步善后处理
+void sigchld_handler(int signo)
+{
+	if (SIGCHLD != signo)
+	{
+		return;
+	}
+	// 等待已退出的所有子进程
+	pid_t pid {};
+	int status = 0;
+	do
+	{
+		pid = waitpid(-1, &status, WNOHANG);
+		cout << "pid exit:" << pid << endl;
+	} while (pid > 0);
 }
