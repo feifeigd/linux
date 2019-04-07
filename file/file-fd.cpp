de5d86df7df1ebf4ef362fcac6d3e08034f8d7e8
feifeigd <feifeigd@21cn.com>
2019-04-08 00:35:22 +0800

    FILE* 与fd互转

--- file/file-fd.cpp
+++ file/file-fd.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 using namespace std;
 
+// FILE*转fd
+// int fileno(FILE* stream);
+// fd 转 FILE*
+// FILE* fdopen(int fd, char const* mode);
 int main(){
 	cout << "\nfileno(stdin) = "  << fileno(stdin);
 	cout << "\nfileno(stdout) = " << fileno(stdout);
